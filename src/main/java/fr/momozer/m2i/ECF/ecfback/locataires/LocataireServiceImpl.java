30bbb9b8c828667a822e0b5e2dcd90397e491718
momozer <zerouali.mi@gmail.com>
2022-12-08 15:15:30 +0100

    fix : ajout de logger dans les ServiceImpl de Locataire

--- src/main/java/fr/momozer/m2i/ECF/ecfback/locataires/LocataireServiceImpl.java
+++ src/main/java/fr/momozer/m2i/ECF/ecfback/locataires/LocataireServiceImpl.java
@@ -17,16 +17,20 @@ public class LocataireServiceImpl implements LocataireService {
 
     @Override
     public List<Locataire> findAll() {
+        logger.info("Affiche l'ensemble des locataires existants");
         return locataireRepository.findAll();
     }
 
     @Override
     public Locataire save(Locataire entity) {
+        logger.info("Sauvegarder, enregistre un novueau locataire en fonction des renseignements" +
+                "fournis par l'utilisateur dans le body: " + entity);
         return locataireRepository.save(entity);
     }
 
     @Override
     public Locataire findById(Integer id) {
+        logger.info("Recherche d'un locataire à partir de son id : " + id);
         return locataireRepository.findById(String.valueOf(id)).orElseThrow(() ->{
             logger.warn("findByIdInvalide: "+ id);
             return  new ResponseStatusException(HttpStatus.NOT_FOUND);
@@ -41,26 +45,31 @@ public class LocataireServiceImpl implements LocataireService {
 
     @Override
     public List<Locataire> findByPrenom(String prenom) {
+        logger.info("Recherche d'un locataire à partir de son prenom : " + prenom);
         return locataireRepository.findByPrenom(prenom);
     }
 
     @Override
     public List<Locataire> findByNom(String nom) {
+        logger.info("Recherche d'un locataire à partir de son nom: " + nom);
         return locataireRepository.findByNom(nom);
     }
 
     @Override
     public List<Locataire> findBydateNaissance(String dateNaissance) {
+        logger.info("Recherche d'un locataire à partir de sa date de naissance : " + dateNaissance);
         return locataireRepository.findBydateNaissance(dateNaissance);
     }
 
     @Override
     public List<Locataire> findByMail(String mail) {
+        logger.info("Recherche d'un locataire à partir de son mail : " + mail);
         return locataireRepository.findByMail(mail);
     }
 
     @Override
     public List<Locataire> findByTel(String tel) {
+        logger.info("Recherche d'un locataire à partir de son tel : " + tel);
         return locataireRepository.findByTel(tel);
     }
 }
